5fb3e8d42242175140343a79474a18186943182a
rawo2010 <sht_rew_23@yahoo.co.jp>
2024-10-19 19:43:24 +0900

    add sample1-1

---
+++ Assets/Scenes/Sample-TweenScale/Sample_TweenScale_LitMotion_1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using LitMotion;
+using LitMotion.Extensions;
+
+public class Sample_TweenScale_LitMotion_1 : MonoBehaviour
+{
+    void Start()
+    {
+        // 5秒かけて-2から2に変化する動作をtransformのpositionXと関連付ける.
+        LMotion.Create(-2f, 2f, 5f).BindToPositionX(transform);
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Scenes/Sample-TweenScale/Sample_TweenScale_LitMotion_2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using LitMotion;
+using LitMotion.Extensions;
+
+public class Sample_TweenScale_LitMotion_2 : MonoBehaviour
+{
+    void Start()
+    {
+        LMotion                     　   // LitMotionの静的クラス
+            .Create(-2f, 2f, 5f) 　　    // が5秒かけて-2から2に変化する動作を作る
+            .BindToPositionX(transform); // 動作とtransformのpositionXと関連付ける.
+    }
+
+    void Update()
+    {
+
+    }
+}
+
