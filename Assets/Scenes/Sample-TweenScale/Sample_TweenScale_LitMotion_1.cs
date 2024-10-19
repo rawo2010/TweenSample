b64e9d75c2852d7e6a247b2b59a86b1e66c8e0ba
嗚呼タマ <sht_rew_23@yahoo.co.jp>
2024-10-19 19:45:06 +0900

    Update Sample_TweenScale_LitMotion_1.cs

--- Assets/Scenes/Sample-TweenScale/Sample_TweenScale_LitMotion_1.cs
+++ Assets/Scenes/Sample-TweenScale/Sample_TweenScale_LitMotion_1.cs
@@ -9,10 +9,5 @@ public class Sample_TweenScale_LitMotion_1 : MonoBehaviour
         // 5秒かけて-2から2に変化する動作をtransformのpositionXと関連付ける.
         LMotion.Create(-2f, 2f, 5f).BindToPositionX(transform);
     }
-
-    void Update()
-    {
-        
-    }
 }
 
