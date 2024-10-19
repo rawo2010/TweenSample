26a17e4bace3b16c47d75ac03fcdda1aa486947a
rawo2010 <sht_rew_23@yahoo.co.jp>
2024-10-19 19:59:19 +0900

    コメント追記

--- Assets/Scenes/Sample-TweenScale/Sample_TweenScale_LitMotion_2.cs
+++ Assets/Scenes/Sample-TweenScale/Sample_TweenScale_LitMotion_2.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
-using LitMotion;
-using LitMotion.Extensions;
+using LitMotion;            // class LMotionのために必要です
+using LitMotion.Extensions; // BindToPositionX()のために必要です
 
 public class Sample_TweenScale_LitMotion_2 : MonoBehaviour
 {
