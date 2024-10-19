f7a271f089dc78eac6a2f2a9d5d9e7d3a1b8f6dc
rawo2010 <sht_rew_23@yahoo.co.jp>
2024-10-19 20:13:11 +0900

    立ち上がりもっちり遅く、最後キビっと

---
+++ Assets/Scenes/Sample-Easing/Sample_Easing_LitMotion_1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using LitMotion;
+using LitMotion.Extensions;
+
+public class Sample_Easing_LitMotion_1 : MonoBehaviour
+{
+    void Start()
+    {
+        LMotion.Create(-2f, 2f, 3f)
+            // 速度を徐々に加速させる.
+            .WithEase(Ease.InCirc)
+            .BindToPositionX(transform);
+    }
+}
+
